6042f8e4b568cb6d4bd9de6e63e7a5db4688175e
Aryan <aryanjha813@gmail.com>
2026-04-21 00:03:16 +0530

    Update account controller

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,84 +1,54 @@
-const userModel = require("../models/user.model")
-const jwt = require("jsonwebtoken")
-const tokenBlackListModel = require("../models/blackList.model")
+const accountModel = require("../models/account.model");
 
 
+async function createAccountController(req, res) {
 
-async function authMiddleware(req, res, next) {
+    const user = req.user;
 
-    const token = req.cookies.token || req.headers.authorization?.split(" ")[ 1 ]
+    const account = await accountModel.create({
+        user: user._id
+    })
 
-    if (!token) {
-        return res.status(401).json({
-            message: "Unauthorized access, token is missing"
-        })
-    }
-
-    const isBlacklisted = await tokenBlackListModel.findOne({ token })
-
-    if (isBlacklisted) {
-        return res.status(401).json({
-            message: "Unauthorized access, token is invalid"
-        })
-    }
-
-    try {
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-        const user = await userModel.findById(decoded.userId)
-
-        req.user = user
-
-        return next()
-
-    } catch (err) {
-        return res.status(401).json({
-            message: "Unauthorized access, token is invalid"
-        })
-    }
-}
-async function authSystemUserMiddleware(req, res, next) {
-
-    const token = req.cookies.token || req.headers.authorization?.split(" ")[ 1 ]
-
-    if (!token) {
-        return res.status(401).json({
-            message: "Unauthorized access, token is missing"
-        })
-    }
-
-    const isBlacklisted = await tokenBlackListModel.findOne({ token })
-
-    if (isBlacklisted) {
-        return res.status(401).json({
-            message: "Unauthorized access, token is invalid"
-        })
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-        const user = await userModel.findById(decoded.userId).select("+systemUser")
-        if (!user.systemUser) {
-            return res.status(403).json({
-                message: "Forbidden access, not a system user"
-            })
-        }
-
-        req.user = user
-
-        return next()
-    }
-    catch (err) {
-        return res.status(401).json({
-            message: "Unauthorized access, token is invalid"
-        })
-    }
+    res.status(201).json({
+        account
+    })
 
 }
+
+async function getUserAccountsController(req, res) {
+
+    const accounts = await accountModel.find({ user: req.user._id });
+
+    res.status(200).json({
+        accounts
+    })
+}
+
+async function getAccountBalanceController(req, res) {
+    const { accountId } = req.params;
+
+    const account = await accountModel.findOne({
+        _id: accountId,
+        user: req.user._id
+    })
+
+    if (!account) {
+        return res.status(404).json({
+            message: "Account not found"
+        })
+    }
+
+    const balance = await account.getBalance();
+
+    res.status(200).json({
+        accountId: account._id,
+        balance: balance
+    })
+}
+
 
 module.exports = {
-    authMiddleware,
-    authSystemUserMiddleware
+    createAccountController,
+    getUserAccountsController,
+    getAccountBalanceController
 }
